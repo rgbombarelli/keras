56b5b22e72cba1a1c32bd453fe14d27846f86933
Rafael Gomez-Bombarelli <rgbombarelli@tormes.chem.harvard.edu>
2016-03-24 19:10:37 -0400

    medium update: vae annealer

--- keras/callbacks.py
+++ keras/callbacks.py
@@ -407,6 +407,29 @@ class LearningRateScheduler(Callback):
         K.set_value(self.model.optimizer.lr, lr)
 
 
+class VAEWeightAnnealer(Callback):
+    '''Weight of variational autoencoder scheduler.
+
+    # Arguments
+        schedule: a function that takes an epoch index as input
+            (integer, indexed from 0) and returns a new
+            weight for the VAE (float).
+    '''
+    def __init__(self, schedule, VAE_layer_idx):
+        super(VAEWeightAnnealer, self).__init__()
+        self.schedule = schedule
+        self.VAE_layer_idx = VAE_layer_idx
+
+    def on_epoch_begin(self, epoch, logs={}):
+        layer = self.model.layers[self.VAE_layer_idx]
+        assert hasattr(layer, 'regularizer_scale'), \
+            'Optimizer must have a "regularizer_scale" attribute.'
+        weight = self.schedule(epoch)
+        print(weight)
+        assert type(weight) == float, 'The output of the "schedule" function should be float.'
+        K.set_value(layer.regularizer_scale, weight)
+
+
 class TensorBoard(Callback):
     ''' Tensorboard basic visualizations.
 
